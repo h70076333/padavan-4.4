3ab669aa1f440ce2c6cce4385ae5f091051240b4
h70076333 <h70076222@88.com>
2026-03-09 23:59:49 +0800

    Update defaults.c

--- trunk/user/shared/defaults.c
+++ trunk/user/shared/defaults.c
@@ -580,6 +580,19 @@ struct nvram_pair router_defaults[] = {
 	{ "ss_update_gfwlist", "0" },
 #endif
 
+#if defined(APP_ZEROTIER)
+	/*Zerotier*/
+	{ "zerotier_enable", "1" },
+	{ "zerotier_id", "" },
+	{ "zerotier_nat", "1" },
+	{ "zerotier_secret", "" },
+	{ "zero_staticnum_x", "0" },
+	{ "zero_afykey", "" },
+	{ "zero_afynen", "" },
+	{ "afycx_enable", "0" },
+	{ "gecoac_enable", "0" },
+#endif
+
 	/* DHCP server parameters */
 	{ "dhcp_start", DEF_LAN_DHCP_BEG },	/* First assignable DHCP address */
 	{ "dhcp_end", DEF_LAN_DHCP_END },	/* Last assignable DHCP address */
